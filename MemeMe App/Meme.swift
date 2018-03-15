2af3958830185164e7651c271127fd18ff374df1
pedrochiossi <36369921+pedrochiossi@users.noreply.github.com>
2018-03-14 22:15:03 -0300

    Update Meme.swift
    fixed indentation

--- MemeMe App/Meme.swift
+++ MemeMe App/Meme.swift
@@ -12,11 +12,11 @@ import UIKit
     
 
 struct Meme {
-        var topText: String
-        var bottomText: String
-        var originalImage: UIImage
-        var memedImage: UIImage
-    }
+    var topText: String
+    var bottomText: String
+    var originalImage: UIImage
+    var memedImage: UIImage
+}
     
     
     
